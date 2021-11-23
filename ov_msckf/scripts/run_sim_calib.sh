01204116fc162803903bb7f9b0140454735f0982
ccchu0816 <ccchu0816>
2021-11-23 13:07:54 -0500

    Modified all the sim scripts. tum_vi and euroc_mav scripts. Downloading the others

--- ov_msckf/scripts/run_sim_calib.sh
+++ ov_msckf/scripts/run_sim_calib.sh
@@ -17,89 +17,59 @@ datasets=(
 #    "udel_neighborhood"
 )
 
+# If we want to calibrate parameters
+sim_do_calibration=(
+    "false"
+    "true"
+)
+
+# If we want to perturb the initial state values
+sim_do_perturbation=(
+    "false"
+    "true"
+)
+
 # location to save log files into
-save_path_est="/home/cc/test/openvins_pra/sim_calib/algorithms"
-save_path_gt="/home/cc/test/openvins_pra/sim_calib/truths"
+save_path_est="/home/chuchu/test_ov/openvins_pra/sim_calib/algorithms"
+save_path_gt="/home/chuchu/test_ov/openvins_pra/sim_calib/truths"
 
 #=============================================================
 # Start the Monte Carlo Simulations
 #=============================================================
 
-# Loop through if use fej or not
+# Loop through the datasets
 for h in "${!datasets[@]}"; do
-
-# groundtruth file save location
-filename_gt="$save_path_gt/${datasets[h]}.txt"
-
+# Loop through if we want to calibrate
+for m in "${!sim_do_calibration[@]}"; do
+# Loop through if we want to perturb
+for n in "${!sim_do_perturbation[@]}"; do
 # Monte Carlo runs for this dataset
 for j in {00..02}; do
 
-#===============================================
-#===============================================
-start_time="$(date -u +%s)"
-filename_est="$save_path_est/ov_23_static_groundtruth/${datasets[h]}/estimate_$j.txt"
-roslaunch ov_msckf simulation.launch \
-  seed:="$j" dataset:="${datasets[h]}.txt" \
-  sim_do_calibration:="false" \
-  sim_do_perturbation:="false" \
-  dosave_pose:="true" \
-  path_est:="$filename_est" \
-  path_gt:="$filename_gt" &> /dev/null
-end_time="$(date -u +%s)"
-elapsed="$(($end_time-$start_time))"
-echo "BASH: ${datasets[h]} - static_groundtruth - run $j took $elapsed seconds";
+
+filename_est="$save_path_est/calib_${sim_do_calibration[m]}/perturb_${sim_do_perturbation[n]}/${datasets[h]}/estimate_$j.txt"
+filename_gt="$save_path_gt/${datasets[h]}.txt"
 
 #===============================================
+# Start Monte Carlo Simulations
 #===============================================
 start_time="$(date -u +%s)"
-filename_est="$save_path_est/ov_23_calib_groundtruth/${datasets[h]}/estimate_$j.txt"
 roslaunch ov_msckf simulation.launch \
-  seed:="$j" \
+  seed:="$((10#$j + 1))" \
   dataset:="${datasets[h]}.txt" \
-  sim_do_calibration:="true" \
-  sim_do_perturbation:="false" \
+  sim_do_calibration:="${sim_do_calibration[m]}" \
+  sim_do_perturbation:="${sim_do_perturbation[n]}" \
   dosave_pose:="true" \
   path_est:="$filename_est" \
   path_gt:="$filename_gt" &> /dev/null
 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
-echo "BASH: ${datasets[h]} - calib_groundtruth - run $j took $elapsed seconds";
-
-#===============================================
-#===============================================
-start_time="$(date -u +%s)"
-filename_est="$save_path_est/ov_23_calib_perturbed/${datasets[h]}/estimate_$j.txt"
-roslaunch ov_msckf simulation.launch \
-  seed:="$j" \
-  dataset:="${datasets[h]}.txt" \
-  sim_do_calibration:="true" \
-  sim_do_perturbation:="true" \
-  dosave_pose:="true" \
-  path_est:="$filename_est" \
-  path_gt:="$filename_gt" &> /dev/null
-end_time="$(date -u +%s)"
-elapsed="$(($end_time-$start_time))"
-echo "BASH: ${datasets[h]} - calib_perturbed - run $j took $elapsed seconds";
-
-#===============================================
-#===============================================
-start_time="$(date -u +%s)"
-filename_est="$save_path_est/ov_23_static_perturbed/${datasets[h]}/estimate_$j.txt"
-roslaunch ov_msckf simulation.launch \
-  seed:="$j" \
-  dataset:="${datasets[h]}.txt" \
-  sim_do_calibration:="false" \
-  sim_do_perturbation:="true" \
-  dosave_pose:="true" \
-  path_est:="$filename_est" \
-  path_gt:="$filename_gt" &> /dev/null
-end_time="$(date -u +%s)"
-elapsed="$(($end_time-$start_time))"
-echo "BASH: ${datasets[h]} - static_perturbed - run $j took $elapsed seconds";
+echo "BASH: ${datasets[h]} - calib_${sim_do_calibration[m]} - perturb_${sim_do_perturbation[n]} - run $j took $elapsed seconds";
 #===============================================
 #===============================================
 
-
+done
+done
 done
 done
 
