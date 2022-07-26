71b86b45a9d059cb420e7e8b23ed49160ee73c76
Patrick Geneva <pgeneva@udel.edu>
2022-07-26 10:20:52 -0400

    fix #262 index values

--- ov_init/src/static/StaticInitializer.cpp
+++ ov_init/src/static/StaticInitializer.cpp
@@ -144,7 +144,7 @@ bool StaticInitializer::initialize(double &timestamp, Eigen::MatrixXd &covarianc
   order.clear();
   order.push_back(t_imu);
   covariance = std::pow(0.02, 2) * Eigen::MatrixXd::Identity(t_imu->size(), t_imu->size());
-  covariance.block(3, 3, 3, 3) = std::pow(0.017, 2) * Eigen::Matrix3d::Identity(); // q
+  covariance.block(0, 0, 3, 3) = std::pow(0.02, 2) * Eigen::Matrix3d::Identity(); // q
   covariance.block(3, 3, 3, 3) = std::pow(0.05, 2) * Eigen::Matrix3d::Identity();  // p
   covariance.block(6, 6, 3, 3) = std::pow(0.01, 2) * Eigen::Matrix3d::Identity();  // v (static)
 
