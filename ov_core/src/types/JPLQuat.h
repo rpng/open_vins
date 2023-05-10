e0ba712dde8aadc6eed9774e202723d171ef5f48
Patrick Geneva <pgeneva@udel.edu>
2023-05-10 11:13:43 -0400

    update quat docs, expand some details about manifold types in ov_type summary

--- ov_core/src/types/JPLQuat.h
+++ ov_core/src/types/JPLQuat.h
@@ -35,6 +35,59 @@ namespace ov_type {
  * We recommend that people new quaternions check out the following resources:
  * - http://mars.cs.umn.edu/tr/reports/Trawny05b.pdf
  * - ftp://naif.jpl.nasa.gov/pub/naif/misc/Quaternion_White_Paper/Quaternions_White_Paper.pdf
+ *
+ *
+ * We need to take special care to handle edge cases when converting to and from other rotation formats.
+ * All equations are based on the following tech report @cite Trawny2005TR :
+ *
+ * > Trawny, Nikolas, and Stergios I. Roumeliotis. "Indirect Kalman filter for 3D attitude estimation."
+ * > University of Minnesota, Dept. of Comp. Sci. & Eng., Tech. Rep 2 (2005): 2005.
+ * > http://mars.cs.umn.edu/tr/reports/Trawny05b.pdf
+ *
+ * @section jplquat_define JPL Quaternion Definition
+ *
+ * We define the quaternion as the following linear combination:
+ * @f[
+ *  \bar{q} = q_4+q_1\mathbf{i}+q_2\mathbf{j}+q_3\mathbf{k}
+ * @f]
+ * Where i,j,k are defined as the following:
+ * @f[
+ *  \mathbf{i}^2=-1~,~\mathbf{j}^2=-1~,~\mathbf{k}^2=-1
+ * @f]
+ * @f[
+ *  -\mathbf{i}\mathbf{j}=\mathbf{j}\mathbf{i}=\mathbf{k}
+ *  ~,~
+ *  -\mathbf{j}\mathbf{k}=\mathbf{k}\mathbf{j}=\mathbf{i}
+ *  ~,~
+ *  -\mathbf{k}\mathbf{i}=\mathbf{i}\mathbf{k}=\mathbf{j}
+ * @f]
+ * As noted in @cite Trawny2005TR this does not correspond to the Hamilton notation, and follows the "JPL Proposed Standard Conventions".
+ * The q_4 quantity is the "scalar" portion of the quaternion, while q_1, q_2, q_3 are part of the "vector" portion.
+ * We split the 4x1 vector into the following convention:
+ * @f[
+ *  \bar{q} = \begin{bmatrix}q_1\\q_2\\q_3\\q_4\end{bmatrix} = \begin{bmatrix}\mathbf{q}\\q_4\end{bmatrix}
+ * @f]
+ * It is also important to note that the quaternion is constrained to the unit circle:
+ * @f[
+ *  |\bar{q}| = \sqrt{\bar{q}^\top\bar{q}} = \sqrt{|\mathbf{q}|^2+q_4^2} = 1
+ * @f]
+ *
+ *
+ * @section jplquat_errorstate Error State Definition
+ *
+ * It is important to note that one can prove that the left-multiplicative quaternion error is equivalent to the SO(3) error.
+ * If one wishes to use the right-hand error, this would need to be implemented as a different type then this class!
+ * This is noted in Eq. (71) in @cite Trawny2005TR .
+ * Specifically we have the following:
+ * \f{align*}{
+ * {}^{I}_G\bar{q} &\simeq \begin{bmatrix} \frac{1}{2} \delta \boldsymbol{\theta} \\ 1 \end{bmatrix} \otimes {}^{I}_G\hat{\bar{q}}
+ * \f}
+ * which is the same as:
+ * \f{align*}{
+ * {}^{I}_G \mathbf{R} &\simeq \exp(-\delta \boldsymbol{\theta}) {}^{I}_G \hat{\mathbf{R}} \\
+ * &\simeq (\mathbf{I} - \lfloor \delta \boldsymbol{\theta} \rfloor) {}^{I}_G \hat{\mathbf{R}} \\
+ * \f}
+ *
  */
 class JPLQuat : public Type {
 
@@ -53,7 +106,7 @@ public:
    * quaternion with a quaternion built from a small axis-angle perturbation.
    *
    * @f[
-   * \bar{q}=norm\Big(\begin{bmatrix} 0.5*\mathbf{\theta_{dx}} \\ 1 \end{bmatrix}\Big) \hat{\bar{q}}
+   * \bar{q}=norm\Big(\begin{bmatrix} \frac{1}{2} \delta \boldsymbol{\theta}_{dx} \\ 1 \end{bmatrix}\Big) \otimes \hat{\bar{q}}
    * @f]
    *
    * @param dx Axis-angle representation of the perturbing quaternion
@@ -73,13 +126,13 @@ public:
 
   /**
    * @brief Sets the value of the estimate and recomputes the internal rotation matrix
-   * @param new_value New value for the quaternion estimate
+   * @param new_value New value for the quaternion estimate (JPL quat as x,y,z,w)
    */
   void set_value(const Eigen::MatrixXd &new_value) override { set_value_internal(new_value); }
 
   /**
    * @brief Sets the fej value and recomputes the fej rotation matrix
-   * @param new_value New value for the quaternion estimate
+   * @param new_value New value for the quaternion estimate (JPL quat as x,y,z,w)
    */
   void set_fej(const Eigen::MatrixXd &new_value) override { set_fej_internal(new_value); }
 
