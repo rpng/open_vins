f10ce282facde6261691e43183c3e828ecce3281
Patrick Geneva <pgeneva@udel.edu>
2023-05-13 17:27:43 -0400

    we have only init'ed once we have updated (fix messy viz in beginning)

--- ov_msckf/src/core/VioManager.h
+++ ov_msckf/src/core/VioManager.h
@@ -96,7 +96,7 @@ public:
   void initialize_with_gt(Eigen::Matrix<double, 17, 1> imustate);
 
   /// If we are initialized or not
-  bool initialized() { return is_initialized_vio; }
+  bool initialized() { return is_initialized_vio && timelastupdate != -1; }
 
   /// Timestamp that the system was initialized at
   double initialized_time() { return startup_time; }
