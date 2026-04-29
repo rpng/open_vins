3643485e74735fbb97ca46955aa9a91085a3a05f
mohamed souab <sawamoha20@gmail.com>
2026-04-29 20:37:53 +0200

    Fix ROS2 header includes for image_transport/tf2/cv_bridge
    Use ROS2 .hpp headers in ov_msckf ROS2 codepaths so builds find image_transport, tf2_geometry_msgs, and cv_bridge.

--- ov_msckf/src/ros/ROSVisualizerHelper.h
+++ ov_msckf/src/ros/ROSVisualizerHelper.h
@@ -34,7 +34,7 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
 #include <tf2/transform_datatypes.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #endif
 
 namespace ov_type {
